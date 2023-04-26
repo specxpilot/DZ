5c8302a8ef4efe4306c0d00cea91e2b574dab58e
Дмитрий <specx@bk.ru>
2023-04-26 09:07:36 +0300

    003

---
+++ 003_Console/Program.cs
@@ -0,0 +1,11 @@
+﻿string username = Console.ReadLine();
+if (username == "Маша" )
+{
+    Console.WriteLine("Ура");
+    }
+    else
+    {
+Console.Write("Привет, ");
+Console.WriteLine(username);
+    }
+    
